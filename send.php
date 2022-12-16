facd08e782b90111515ee7621beaf007986eab3f
DmytroLemeshuk <114018127+DimaLemeshuk@users.noreply.github.com>
2022-12-16 20:35:25 +0200

    Add files via upload

---
+++ send.php
@@ -0,0 +1,23 @@
+<?php
+    $fio = $_POST['fio'];
+    $email = $_POST['email'];
+    $fio = htmlspecialchars($fio);
+    $email = htmlspecialchars($email);
+    $fio = urldecode($fio);
+    $email = urldecode($email);
+    $fio = trim($fio);
+    $email = trim($email);
+    echo $fio;
+    echo "<br>";
+    echo $email;
+    echo "<br>";
+    //if (mail("lemesukdima8@gmail.com", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: dmutrolemeshuk@gmail.com \r\n"))
+    if (mail("lemesukdima8@gmail.com", "TEMA", "rrbbfeebfbfbf" ,"From: lemesukdima8@gmail.com \r\n"))
+    //From: info@localhost:8888/site2/ \r\n
+    //From: info@localhost:8888/site2/send.php
+    {     
+        echo "сообщение успешно отправлено";
+    } else {
+        echo "при отправке сообщения возникли ошибки";
+    }
+?>
